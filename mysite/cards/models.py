e124b09ae29586e2c6a9a33e68905ed4a9d84508
Oprisor Andrei <oprisor_andrei@yahoo.com>
2015-07-23 13:28:48 +0300

    added views

--- mysite/cards/models.py
+++ mysite/cards/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Cards(models.Model):
+
+	owner = models.ForeignKey(User)
 	url=models.URLField()
 	name=models.CharField(max_length=60)
 	image=models.ImageField()
 	media_type=models.CharField(max_length=32)
 	create_at=models.DateTimeField('date published')
+    
+	def __str__(self):
+		return self.name
